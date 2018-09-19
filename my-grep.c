dea4cb40d077b4b3ae113d3da113a4957fa91ada
kalyssao <klysssa@icloud.com>
2018-09-19 14:15:37 -0700

    final changes

--- my-grep.c
+++ my-grep.c
@@ -3,17 +3,14 @@
 #include <stdlib.h>
 #define MAXCHAR 1000
 
-int iterateOnEntry
-
 int main(int argc, char *argv[]) {
 	int j;
-	int found = 0;
 	char temp[MAXCHAR];
 	char user[MAXCHAR];
 	
 	FILE *fileToSearch;
-	FILE *userEntry
-
+	FILE *userEntry;
+	
 	/* If there's no argument */ 
 	if( argc <= 1) {
 		printf("my-grep: searchterm [file ...]\n");
@@ -23,21 +20,45 @@ int main(int argc, char *argv[]) {
 	/* Else if there's an argument but no file*/
 	else if( argc == 2) {
 		printf("Enter the text file: ");
-		userEntry = fgets(user, MAXCHAR, stdin);
+		fgets(user, MAXCHAR, stdin);
+
+		/* strip the newline character at the end */
+		char* nl = strchr(user, '\n');
+		if( nl != NULL) {
+			*nl = '\0';		
+		}		
+		
+		userEntry = fopen(user,"r");
+
+		if( userEntry == NULL) {
+			printf("my-grep: cannot open file\n");
+			exit(1);
+		}
+		else {
+				
+			/* whilst there's text to search, search it */
+			while( fgets( temp, MAXCHAR, userEntry) != NULL) {
+					
+				/*search each line of the text file for the argument*/ 
+				if( (strstr(temp, argv[1])) != NULL) {
+				
+					/* if it's there, print the line */
+					printf("%s", temp);
+				}	
+			}
+		}		
 	}
 	
-	/* if empty string */
-
 	/* Else if there are arguments */
 	else {
 
 		/* Check the length of the argv for items */
 		for( j = 2; j < argc; ++j) {
-			fileToSearch = fopen(argv[j],"r");	\
+			fileToSearch = fopen(argv[j],"r");
 
 			/* Can it be opened? */			
 			if( fileToSearch == NULL) {
-				printf("my-grep: cannot open file \n");
+				printf("my-grep: cannot open file\n");
 				exit(1);	
 			}
 	
@@ -48,22 +69,14 @@ int main(int argc, char *argv[]) {
 					
 					/*search each line of the text file for the argument*/ 
 					if( (strstr(temp, argv[1])) != NULL) {
-					
+						
 						/* if it's there, print the line, increment counter */
 						printf("%s", temp);
-						found++;
 					}
-					
 				}
-				
-				if( found == 0) {
-					printf("Argument cannot be found \n");
-				}
-			
-			}
+			}			
 			fclose(fileToSearch);
-			
 		}
 	}
-return(0);
+	return(0);
 }
