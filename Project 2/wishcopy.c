9fe934daeedc97147e96ad204d84f0eab584cc0e
kalyssao <klysssa@icloud.com>
2018-10-10 20:27:10 -0700

    progress made so far and resources

--- Project 2/wishcopy.c
+++ Project 2/wishcopy.c
@@ -3,14 +3,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 
 char error_message[30] = "An error has occurred\n";
 
 //execute takes the whole line array and its size and uses the size to test parameter length
-int executeBuiltIn( char *argv[], int size) {
-	char **pathArray = malloc(0);
+int executeBuiltIn( char *argv[], int size, char *paths[]) {
+
+	int j = 1;
+	int index = 0;
+	
+	char **pathNames = NULL;
+	char str[ 1024 ];
+
 	int ret;
+	int len;
 	//check if the command is exit, and if a parameter was passed to it or nah
 	if( !strcmp( "exit", argv[0])) {
 		if( size == 1) {
@@ -20,32 +28,41 @@ int executeBuiltIn( char *argv[], int size) {
 			write(STDERR_FILENO, error_message, strlen(error_message));
 		}
 	}
-
+	//SLICE USER ENTRY ARRAY INTO PATH ARRAY AND COPY TO GLOBAL
 	//check if the command is path, and if there are no, one or more args
 	if( !strcmp( "path", argv[0])) {
+		pathNames = realloc(pathNames, sizeof(char*)*(index+1));
+
 		if( size == 1) { //no argument
 			//shell should not be able to run any programs
+			len = 0;
+			//printf("The paths are %s\n", pathArray)
 		}
 
-		if( size == 2) { //one argument - string copy
-			//strcpy(pathArray, argv[1]);
-			pathArray = realloc(pathArray, size * sizeof (char*));
-			pathArray[size - 1] = malloc(100 * sizeof(char));
-			strcpy(pathArray[size - 1], argv[1]);
+		if( size == 2) { //one argument - add arg to path
+			
+			char *dup = malloc(strlen(argv[1])+1);
+			strcpy(dup,argv[1]);
+			pathNames[index++] = dup;
 					
+			len = 1;
+			printf("The paths are %s\n", &pathNames);			
 		}
 
 		if( size >= 2) { //two arguments - string copy and concatenate the rest
 			int i;
-			pathArray = realloc(pathArray, size * sizeof (char *));
-			strcpy(pathArray[size - 1], argv[1]);
+			len = size - 1;
+//to size or size-1? is it null terminated?
+			for( i = 1; i < size; ++i) {
+				char *dup = malloc(strlen(argv[i])+1);
+				strcpy(dup, argv[i]);
+				strcat(dup, " ");
+				pathNames[index++] = dup;
 
-			for( i = 2; i < size; ++i) {
-				pathArray = realloc(pathArray, size * sizeof (char*));
-				pathArray[size - 1] = malloc(100 * sizeof(char));
 				strcat(pathArray[size - 1] , " ");	
 				strcat(pathArray[size - 1], argv[i]);
 			}
+			printf("The paths are %s\n", &pathArray);
 		}
 	}
 	
@@ -55,18 +72,21 @@ int executeBuiltIn( char *argv[], int size) {
 			write(STDERR_FILENO, error_message, strlen(error_message)); 
 		}
 		else {	
-			printf("Executing %s on command %s", argv[0], argv[1]);
-			gdir = 
-			for item in path array, check if chdir works for a given path
-			ret = chdir(argv[1]);
-			printf("%i", ret);
-			if( ret != 0) {
-				write(STDERR_FILENO, error_message, strlen(error_message));
-			}
-			else {
-				//successful!
-				printf("Changed directory to %s\n", argv[1]);
-				return 1;
+			//printf("Executing %s on command %s", argv[0], argv[1]);
+
+			for( int p = 0; p < len - 1; ++p) {
+				inter = strcat(path[p], "/");
+				dir = strcat(inter, argv[1]);
+				ret = chdir(dir);
+			
+				if( ret != 0) {
+					continue;
+				}
+				else {
+					//successful!
+					printf("Changed directory to %s\n", argv[1]);
+					return len;
+				}
 			}
 		}
 	}
@@ -79,15 +99,10 @@ int executeBuiltIn( char *argv[], int size) {
 
 int main( int argc, char *argv[]) {
 	FILE *tokenFile;
-
-	/*char str[ 1024 ];
-	char *lastToken = NULL;
-	char **array = malloc(0);*/
-	char *builtIns[] = {"exit", "cd", "path"};
-	
-	//int ret;
 	int j = 1;
 	int index = 0;
+	
+	char *builtIns[] = {"exit", "cd", "path"};
 	char **res = NULL;
 	char str[ 1024 ];
 
@@ -99,58 +114,60 @@ int main( int argc, char *argv[]) {
 
 	// if argc is 2, batch mode
 	if( argc == 2) {
-		//printf("%i\n",argc);
 		tokenFile = fopen(argv[j], "r");
 		
-		/* Check if the file doesn't exist */
+		// Check if the file doesn't exist
 		if( tokenFile == NULL) {
 			//bad batch file
 			write(STDERR_FILENO, error_message, strlen(error_message)); 
 			exit(1);
 		}
-		/* If it does, open it, read the line and tokenize it */
+		// If it does, open it, read the line and tokenize it
 		else {
 			while( fgets( str, 1024, tokenFile) != NULL) {
-				//print line to stdout to confirm	
-				//puts( str, stdout);
-
 				char *lastToken = strtok( str, " "); 
-				//check return value of strtok
+				
 				while( lastToken != NULL) {
-					//size++;
 					res = realloc(res, sizeof(char*)*(index+1));
 					char *dup = malloc(strlen(lastToken)+1);
 					strcpy(dup,lastToken);
 					res[index++] = dup;
-
-					/*array = realloc(array, size * sizeof (char*));
-					array[size - 1] = malloc(100 * sizeof(char));
-					strcpy(array[size - 1], lastToken);
-					*/
+					
 					lastToken = strtok( NULL, " ");
 					
 				}
+				
 				res = realloc(res, sizeof(char*)*(index+1));
 				res[index] = NULL;
+
 				int size = index;
 
-				/*for( int k = 0; res[k]; ++k){				
-					printf("%s\n", res[k]);
-				}*/
 				
-				//iterate through builtins - check if command is builtin
+				//check if command is builtin
 				for( int i = 0; i < 3; ++i) {
-					//if they arent equal, continue
+					//if current isn't, continue
 					if( strcmp(res[0], builtIns[i]) != 0) {
 						continue;
 					}
-					//else, call the line array and execute the builtin
-					//printf("executing builtin..%s\n", res[0]);
+					//else, execute the builtin
 					executeBuiltIn(res, size);
 					break;
 				}
 
-				//non-builtin: create child process and do plenty things. 			
+				//if we're here - its non-builtin
+				//create child process and do things. 
+				pid_t child_pid = fork();
+				//child process
+				if( child_pid == 0) {
+					//for item in path, execute the function in 
+					for( int p = 0; p < pathLen; ++p) {
+						inter = strcat(path[i], "/");
+						dir = strcat(inter, res[0]);
+						if( execv(dir, res)) {
+							//error
+						} 
+					}
+				}
 			}
 			//check if EOF marker has been reached
 			if( feof(tokenFile)) {
