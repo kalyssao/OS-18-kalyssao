9e7752c1851db8bbd99c33175527d987713a4b03
Kalyssa <klysssa@icloud.com>
2018-10-12 15:23:27 -0700

    no error code - segmentation faults though -_-

--- Project 2/wissssh.c
+++ Project 2/wissssh.c
@@ -9,17 +9,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 //define global arrays for commands and the path - should they initially be empty?
 
 char path[900];
-char commands[100];
+char *commands[100];
 
 char error_message[30] = "An error has occurred\n";
 int commandLen = 0;
 
 void addCommand(char *argv);
-int checkBuiltIn(char *argv[]);
+int checkBuiltIn(char argv[]);
 int executeOther();
 void executeBuiltIn(int);
 int batchMode(char *argv[]);
@@ -31,23 +33,23 @@ void addCommand(char *argv) {
     commandLen = 0; //set command length to 0
 
     while(argv != NULL){
-        commands[i] = argv[j];
+        *(commands[i]) = argv[j];
         commandLen++;   //increment command length as there are values to add
 		j++;
     }
     return; 
 }
 
-int checkBuiltIn(char *argv[]){
-    if(!strcmp(argv[0], "exit")){
+int checkBuiltIn(char argv[]){
+    if(!strcmp(&argv[0], "exit")){
         puts("exiting...");
         return 10;
     }
-    if(!strcmp(argv[0], "cd")){
+    if(!strcmp(&argv[0], "cd")){
         puts("changing directory...");
         return 9;
     }
-    if(!strcmp(argv[0], "path")){
+    if(!strcmp(&argv[0], "path")){
         puts("changing path...");
         return 8;
     }
@@ -62,7 +64,7 @@ void executeBuiltIn(int a) {
     int j;
     int retCH;
     
-    char dir[100];
+    //char dir[100];
     
     if(a == 10) { //exit
         exit(0);
@@ -77,21 +79,25 @@ void executeBuiltIn(int a) {
 	//chigga ain't so lit anymore :-(
 	//result /bin/cat
     if(a == 9) { //cd
-        while(path[i] != NULL) { //raises type mismatch error
-            strcpy(dir, path[i]);
+		if(commandLen == 2) {
+	        //while(*path[i] != NULL) {
+			retCH = chdir(commands[1]);
+            /*strcpy(dir, path[i]);
             strcat(dir, "/");
             strcat(dir, commands[0]); //parameter count does not agree with previous definition
-			//CHECK IF IT WORKSSSSSSSSSSS
-            retCH = chdir(dir);
+			//CHECK IF IT WORKSSSSSSSSSSS*/
+            //retCH = chdir(dir);
 
             if(retCH != 0) {
                 //failure - incorrect path maybe?
-                i++;
-                continue;
+                //i++;
+                //continue;
+				write(STDERR_FILENO, error_message, strlen(error_message));
+				exit(1);
             }
             else {
                 //change was successful - break
-                printf("Successfully changed directory to %s\n", dir);
+                printf("Successfully changed directory to %s\n", commands[1]);
             }
         }
         write(STDERR_FILENO, error_message, strlen(error_message));
@@ -101,25 +107,25 @@ void executeBuiltIn(int a) {
     if(a == 8) {  //path - check if zero or more
         
 		if(commandLen == 1) { //no arg - run nothing except builtin
-            path[0] = NULL; //should the whole path be equated to NULL?
+            path[0] = '\0'; //should the whole path be equated to NULL?
             puts("You can only run builtins");
         }
         
 		if(commandLen == 2) { //one arg - overwrite current path
 		//could use an assignment, commands[i] = new_path
 		//make others NULL
-            path[0] = commands[i];// = strcpy(path, commands[1]);
+            path[0] = *(commands[i]);// = strcpy(path, commands[1]);
         }
 		//path = 0 /bin 1 /binbin NULL
 		//command = path /stuff /path
 		//commandLen = 3
         else {
-            while(path[i] != NULL) {  //two for loops - first is
+            while(path[i] != '\0') {  //two for loops - first is
                 for(j = 1; j < commandLen; ++j) { //at index 1, the first path to be copied
-                    path[i] = commands[j]; //overwriting the contents of the path
+                    path[i] = *(commands[j]); //overwriting the contents of the path
                     i++;
                 }
-                path[i] = NULL;
+                path[i] = '\0';
             }
         }
     }
@@ -129,26 +135,33 @@ void executeBuiltIn(int a) {
 int executeOther(){
     int i;
     int retEO;
+	
     pid_t c_pid;
 
     c_pid = fork();
-    while(path[i] != NULL) {
-        if(access(path[i], X_OK) != 0) {
-            write(STDERR_FILENO, error_message, strlen(error_message));
-        }
-        else {
-            retEO = execvp(&commands[0], commands); //warning pass argument two from incompatible type
+    while(path[i] != '\0') {
+		if(c_pid == 0) {
+		    if(access(&path[i], X_OK) != 0) {
+		        write(STDERR_FILENO, error_message, strlen(error_message));
+		    }
+		    else {
+		        retEO = execvp(commands[0], commands); 
 
-            if(retEO == -1) {
-                i++;
-                continue;
-                
-            }
+		        if(retEO == -1) {
+		            i++;
+		            continue;
+		            
+		        }
 
-            else {
-                return 0; //successful execution
-            }
-        }
+		        else {
+		            return 0; //successful execution
+		        }
+		    }
+		}
+		else {
+			wait(NULL);
+			return 1;
+		}
     }
     write(STDERR_FILENO, error_message, strlen(error_message));
     //printf("couldn't execute command %s\n", commands[0]);
@@ -190,18 +203,22 @@ int interactiveMode(){
             retEO = executeOther(); //execute non builtin
             if(retEO == 0) {
                 puts("Success!");
-                continue;
+                return 0;
             }
             else {
-                break;
+                return 1;
             }
         }
+		/*if(&commands[0] == "exit") {
+			write(STDERR_FILENO, error_message, strlen(error_message)); 
+			exit(1);
+		}*/
     }
 }
 
 //btchmode takes an array, attempts to open a file and
 int batchMode(char *argv[]) {
-    //int retCOM;
+    int retO;
     int retBI;
     FILE *tokenFile;
 
@@ -236,21 +253,26 @@ int batchMode(char *argv[]) {
             }
 
             else {
-                executeOther(); //execute non builtin
+                retO = executeOther(); //execute non builtin
+				if (retO == 0) { //success
+					return 0;
+				}
+				break;
             }
 
             commandLen = 0;
             return 0; //return success
         }
-    
     }
+	write(STDERR_FILENO, error_message, strlen(error_message));
+	exit(1);
 }
 
-void main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 	//initialise path[0] to bin, path[1] = NULL
 	
-    int retB = ;//batch mode return
-    int retI = ; //interactive mode return
+    int retB;//batch mode return
+    int retI; //interactive mode return
     while(1) {
         if(argc == 1) {
             puts("entering interactive mode..");
@@ -268,7 +290,7 @@ void main(int argc, char *argv[]) {
     
         if(argc == 2) {
             puts("entering batch mode...");
-            retB = batchMode(argv[]);
+            retB = batchMode(argv);
     
             if(retB == 0 ) {
                 puts("Success!");
