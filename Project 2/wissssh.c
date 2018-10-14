690c943e52b62c919b1abb2cc5f02b1015fe788c
Kalyssa <klysssa@icloud.com>
2018-10-14 00:32:31 -0700

    almost there..

--- Project 2/wissssh.c
+++ Project 2/wissssh.c
@@ -14,39 +14,51 @@
 
 //define global arrays for commands and the path - should they initially be empty?
 
-char path[900];
-char *commands[100];
+char path[900] = "/bin";
+
+char *commands[900];
 
 char error_message[30] = "An error has occurred\n";
 int commandLen = 0;
 
-void addCommand(char *argv);
+int addCommand(char *argv);
+int addCommands(char *argv[], int tokenLen);
 int checkBuiltIn(char argv[]);
 int executeOther();
 void executeBuiltIn(int);
 int batchMode(char *argv[]);
 int interactiveMode();
 
-void addCommand(char *argv) {
-    int i = 0;
+int addCommands(char *argv[], int tokenLen) {
 	int j = 0;
-    commandLen = 0; //set command length to 0
-
-    while(argv != NULL){
-        *(commands[i]) = argv[j];
-        commandLen++;   //increment command length as there are values to add
+	for (int i = 0; i < tokenLen; ++i) {
+		commands[j] = argv[i];
 		j++;
-    }
-    return; 
+	}
+	commandLen = tokenLen;
+	printf("%s successfully added to commands array\n", commands[0]);
+	return 0; //return success
+}
+
+int addCommand(char *argv) { //adds a single command
+	//printf("Added command %s to the array\n", argv);
+	//return;
+    //int i = 0;
+	//int j = 0;
+    commandLen = 0; //set command length to 0
+    //while(argv){
+    commands[0] = argv;
+	printf("Successfully added command %s\n", commands[0]);
+    commandLen++;   //increment command length as there are values to add
+	
+    return 0; //return success
 }
 
 int checkBuiltIn(char argv[]){
     if(!strcmp(&argv[0], "exit")){
-        puts("exiting...");
         return 10;
     }
     if(!strcmp(&argv[0], "cd")){
-        puts("changing directory...");
         return 9;
     }
     if(!strcmp(&argv[0], "path")){
@@ -63,7 +75,7 @@ void executeBuiltIn(int a) {
     int i = 0;
     int j;
     int retCH;
-    
+    printf("command length is %d\n", commandLen);
     //char dir[100];
     
     if(a == 10) { //exit
@@ -98,12 +110,14 @@ void executeBuiltIn(int a) {
             else {
                 //change was successful - break
                 printf("Successfully changed directory to %s\n", commands[1]);
+				return;
             }
         }
+		//more or less than 1 parameter
         write(STDERR_FILENO, error_message, strlen(error_message));
-        //printf("couldn't execute command %s\n", commands[0]);
+		return;
     }
-    //enquiring minds want to know! - commandLen var in which function?
+
     if(a == 8) {  //path - check if zero or more
         
 		if(commandLen == 1) { //no arg - run nothing except builtin
@@ -114,11 +128,11 @@ void executeBuiltIn(int a) {
 		if(commandLen == 2) { //one arg - overwrite current path
 		//could use an assignment, commands[i] = new_path
 		//make others NULL
-            path[0] = *(commands[i]);// = strcpy(path, commands[1]);
+			
+            //path[0] = commands[1];// = 
+			path[0] = *commands[1];
         }
-		//path = 0 /bin 1 /binbin NULL
-		//command = path /stuff /path
-		//commandLen = 3
+
         else {
             while(path[i] != '\0') {  //two for loops - first is
                 for(j = 1; j < commandLen; ++j) { //at index 1, the first path to be copied
@@ -132,136 +146,189 @@ void executeBuiltIn(int a) {
 }
 
 //if not builtin - execute
-int executeOther(){
-    int i;
+int executeOther() {
+    int i = 0;
     int retEO;
-	
+	printf("executing other command %s with length %d\n", commands[0], commandLen);
     pid_t c_pid;
 
     c_pid = fork();
-    while(path[i] != '\0') {
-		if(c_pid == 0) {
-		    if(access(&path[i], X_OK) != 0) {
-		        write(STDERR_FILENO, error_message, strlen(error_message));
-		    }
-		    else {
-		        retEO = execvp(commands[0], commands); 
+    if(c_pid == 0) {
+		while(path[i] != '\0') {
+			if(access(&path[i], X_OK) != 0) { //path is legit
+				    i++;
+					continue;		
+			}
+			else {
+				puts("about to execvp..");
+				retEO = execvp(commands[0], commands); //execvp given command
+				
+				if(retEO == -1) {
+					puts("there was a problem..");	    
+				}
 
-		        if(retEO == -1) {
-		            i++;
-		            continue;
-		            
-		        }
-
-		        else {
-		            return 0; //successful execution
-		        }
-		    }
+				else {
+					puts("success");
+					return 0; //successful execution
+				}
+			}	
 		}
-		else {
-			wait(NULL);
-			return 1;
-		}
-    }
-    write(STDERR_FILENO, error_message, strlen(error_message));
-    //printf("couldn't execute command %s\n", commands[0]);
+	}
+	else {
+		wait(NULL);
+		return 1;
+	}
+	// write(STDERR_FILENO, error_message, strlen(error_message));
     return 1;
 }
 
 //one argument means interactive mode
-int interactiveMode(){
+int interactiveMode() {
     char *input = NULL;
     size_t len = 0;
+	char *tokenArray[1024];
 
-    int i = 1;
+	int k = 0;
     int retBI;
     int retEO;
+	int retAC;
+	int tokenLen;
 
     while(1) { // check for exit entry for while loop 
         fputs("wish> ", stdout);
         while(getline(&input, &len, stdin) != -1) { //while line
+			printf("input read is %s", input);
+			char* p = strchr(input,'\n');
+    			if (p) {
+      				*p = '\0';
+					//tokenize first item to commands array
+            		char *lastToken = strtok( input, " "); 
+					tokenLen++;
+            		printf("token is %s\n", lastToken);//addCommand(lastToken);
 
-            //tokenize first item to commands array
-            char *lastToken = strtok( input, " "); 
-            //addCommand(lastToken);
+                    //while there are more words to tokenize
+                    while( lastToken != NULL ) {
+						tokenArray[k] = lastToken;
+						k++;
+						tokenLen++;
+						lastToken = strtok( NULL, " "); //rest of the arguments
+						
+					}
+                    	/* addCommand(lastToken); //add command to command array*/
+					//after tokenizing line, add to commands array
+					retAC = addCommands(tokenArray, tokenLen);
 
-                        //while there are more words to tokenize
-                        while( lastToken != NULL) {
-                                addCommand(lastToken); //add command to command array
-                                lastToken = strtok( NULL, " "); //next token - arguments
-                                i++;
-                        }
-        }
-		
-                printf("command is %s\n", commands[0]);
-        retBI = checkBuiltIn(commands[0]);
-        if( retBI != 7){
-            executeBuiltIn(retBI); //re
-            continue;
-        }
-        else {
-            retEO = executeOther(); //execute non builtin
-            if(retEO == 0) {
-                puts("Success!");
-                return 0;
-            }
-            else {
-                return 1;
-            }
-        }
-		/*if(&commands[0] == "exit") {
-			write(STDERR_FILENO, error_message, strlen(error_message)); 
-			exit(1);
-		}*/
-    }
+					if(retAC != 0) {
+						write(STDERR_FILENO, error_message, strlen(error_message));
+						break;
+					}
+					else {
+						printf("command is %s\n", commands[0]);
+        				retBI = checkBuiltIn(commands[0]);
+
+        				if( retBI != 7){
+            				executeBuiltIn(retBI); //execute builtin depending on return value
+            				//continue;
+        				}
+
+						else {
+							puts("executing other..\n");
+							retEO = executeOther(); //execute non builtin
+							if(retEO == 0) {
+								puts("Success!");
+								return 0;
+							}
+							else {
+								return 1;
+							}
+						}
+	        		}
+				}
+				else { //eof error	
+					exit(0);
+					//write(STDERR_FILENO, error_message, strlen(error_message)); 
+				}
+		}
+	
+	}
+	return 0;
 }
+            
 
-//btchmode takes an array, attempts to open a file and
+//batchmode takes an array, attempts to open a file and
 int batchMode(char *argv[]) {
     int retO;
     int retBI;
+	int retAC;
+	int tokenLen;
+	int k = 0;
+
+
     FILE *tokenFile;
 
+	char *tokenArray[1024];
     char str[1024];
-    printf("Opening file named %s\n", argv[1]);
+
     tokenFile = fopen(argv[1], "r");
+	printf("Opening file named %s\n", argv[1]);
     
     // Check if the file doesn't exist
     if( tokenFile == NULL) {
             //bad batch file
             write(STDERR_FILENO, error_message, strlen(error_message)); 
-            exit(1);
+			exit(1);
     }
 
         // If it exists, open it, read the line and tokenize it
     else {
         while( fgets( str, 1024, tokenFile) != NULL) {  //first line
-            char *lastToken = strtok( str, " "); 
+			char* p = strchr(str,'\n');
+    		if (p) {
+      			*p = '\0';
+		        char *lastToken = strtok( str, " "); 
 
-            //while there are words to tokenize
-            while( lastToken != NULL) {
-                addCommand(lastToken); //add command to command array
-                lastToken = strtok( NULL, " "); //next token - arguments
-            }
-
-            // end of line - check first command for builtin
-            retBI = checkBuiltIn(commands[0]);
-
-            if( retBI != 7) {
-                executeBuiltIn(retBI);
-                continue;
-            }
-
-            else {
-                retO = executeOther(); //execute non builtin
-				if (retO == 0) { //success
-					return 0;
+		        //while there are words to tokenize
+		        while( lastToken != NULL) {
+						tokenArray[k] = lastToken;
+						tokenLen++;	
+						k++;
+						lastToken = strtok( NULL, " "); //rest of the arguments
+							            
+		        }
+				retAC = addCommands(tokenArray, tokenLen);
+				if(retAC != 0) {
+						write(STDERR_FILENO, error_message, strlen(error_message));
+						break;
 				}
-				break;
-            }
+				else {
+					printf("command is %s\n", commands[0]);
+				    // end of line - check first command for builtin
+				    retBI = checkBuiltIn(commands[0]);
 
-            commandLen = 0;
-            return 0; //return success
+				    if( retBI == 7) {	//execute non builtin
+
+						retO = executeOther(); 
+
+						if (retO == 0) { //success
+							return 0;
+						}
+						printf("error executing %s\n", commands[0]);
+						break;				   
+					 }
+
+				    else {
+						executeBuiltIn(retBI);
+				        continue;
+				    }
+
+				    commandLen = 0;
+				    return 0; //return success
+				}
+			}
+			else { //eof error	
+				exit(0);
+				//write(STDERR_FILENO, error_message, strlen(error_message)); 
+			}
         }
     }
 	write(STDERR_FILENO, error_message, strlen(error_message));
@@ -273,37 +340,40 @@ int main(int argc, char *argv[]) {
 	
     int retB;//batch mode return
     int retI; //interactive mode return
-    while(1) {
-        if(argc == 1) {
-            puts("entering interactive mode..");
-            retI = interactiveMode();
-    
-            if(retI == 0) {
-                puts("Interactive mode successful");
-                continue;
-            }
-    
-            else{
-                break;
-            }
-        }
-    
+    //while(1) {
+		if(argc == 1) {
+			while(1){
+				puts("entering interactive mode..");
+		        retI = interactiveMode();
+		
+		        if(retI == 0) {
+			        puts("Interactive mode successful");
+		            continue;
+		        }
+		
+		        else{
+		            break;
+		        }
+			}
+    	}
         if(argc == 2) {
-            puts("entering batch mode...");
-            retB = batchMode(argv);
-    
-            if(retB == 0 ) {
-                puts("Success!");
-                continue;
-            }
-            else {
-                break;
-            }
+			while(1) {
+		        puts("entering batch mode...");
+		        retB = batchMode(argv);
+		
+		        if(retB == 0 ) {
+		            puts("Success!");
+		            continue;
+		        }
+		        else {
+		            break;
+		        }
+			}
         }
     
         else {
             write(STDERR_FILENO, error_message, strlen(error_message));
-            break; //return failure
+            return 1; //return failure
         }
-    }
+	//}
 }
